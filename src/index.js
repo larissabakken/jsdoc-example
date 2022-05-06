00086e93fed1d19bbd3c504bed7ab18e138b7f17
Larissa Ramos <devlarissasr@gmail.com>
2022-05-06 13:02:19 -0300

    add

--- src/index.js
+++ src/index.js
@@ -6,25 +6,45 @@
 
 /**
  * Student Name
- * @type {string}
+ * @type {string} 
  */
 const studentName = "John Doe";
 
 /**
  * Array of grades
- * @type {Array <number>}
+ * @type {Array <number> <string>}
  */
-const grades = [9, 1, 15, 5, 95];
+const grades = [9, 1, 15, 5, 95, 'string'];
 
+/**
+ *  Todo object 
+ *  @type { {id: number ; text: number|string}}
+ */
 const todo = {
     id: 1,
     text: "listen to music",
 };
 
-const calculateTax = (amount, tax) => {
+/**
+ * Calculate tax
+ * @param {number} amount - Total amount
+ * @param {number} tax - Tax percentage
+ * @returns {string} - Total with a dollar sign
+ */
+ const calculateTax = (amount, tax) => {
     return `$${amount + tax * amount}`;
-};
+  };
 
+
+
+/**
+ * A employee
+ * @typedef {Object} Employee
+ * @property {number} id - employee ID
+ * @property {string} name - employee name
+ * @property {string|number} [age] - employee age (optional)
+ * @property {boolean} isActive - employee is active
+ */
 const employee = {
     id: 1,
     name: "John Doe",
